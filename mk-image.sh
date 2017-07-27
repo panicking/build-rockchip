5d5bdabfebb7392a2eda67ecf2c246655416a41f
Jacob Chen <jacob-chen@iotwrt.com>
2017-07-27 20:11:40 +0800

    mk-image: spefic uuid for rootfs partiton
    prepare to switch kernel cmdline to
    
    root=PARTUUID=b921b045-1d
    root=PARTUUID=69dad710-2c
    
    to avoid the using of mmc index
    
    Signed-off-by: Jacob Chen <jacob-chen@iotwrt.com>

--- mk-image.sh
+++ mk-image.sh
@@ -89,6 +89,21 @@ generate_system_image() {
 	parted -s ${SYSTEM} set 6 boot on
 	parted -s ${SYSTEM} unit s mkpart rootfs ${ROOTFS_START} 100%
 
+	if [ "$CHIP" == "rk3328" ] || [ "$CHIP" == "rk3399" ]; then
+		ROOT_UUID="B921B045-1DF0-41C3-AF44-4C6F280D3FAE"
+	else
+		ROOT_UUID="69DAD710-2CE4-4E3C-B16C-21A1D49ABED3"
+	fi
+
+	gdisk ${SYSTEM} <<EOF
+x
+c
+7
+${ROOT_UUID}
+w
+y
+EOF
+
 	# burn u-boot
 	if [ "$CHIP" == "rk3288" ] || [ "$CHIP" == "rk322x" ] || [ "$CHIP" == "rk3036" ]; then
 		dd if=${OUT}/u-boot/idbloader.img of=${SYSTEM} seek=${LOADER1_START} conv=notrunc
